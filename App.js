af26e12bcd09801b2fcfff1b555288a507f377e4
Noora Keskimäki <noora.keskimaki@gmail.com>
2022-08-30 14:33:31 +0300

    first commit, app ready

--- App.js
+++ App.js
@@ -1,10 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Alert, Button } from 'react-native';
 
 export default function App() {
+
+  const [number, setNumber] = React.useState(null);
+  const [bodyText, setBodyText] = React.useState('Guess a number between 1-100');
+  const [secretNumber, setSecretNumber] =React.useState(Math.floor(Math.random() * 100) + 1);
+  const [counter, setCounter] = React.useState(0);
+  const buttonPressed = () => {
+
+    if (number > 100 || number<1) {
+
+      Alert.alert('The number must be between 1-100')
+
+    }
+    
+    else if (number === secretNumber) {
+
+      Alert.alert('You guessed the number in '+counter+ ' guesses')
+      setSecretNumber(Math.floor(Math.random() * 100) + 1)
+      setBodyText('Guess a number between 1-100');
+    
+    } else if (number > secretNumber){
+
+      setBodyText('Your guess ' + number + ' is too high')
+      setCounter(counter+1)
+      
+      
+    }
+
+    else if (number < secretNumber){
+
+      setBodyText('Your guess ' + number + ' is too low')
+      setCounter(counter+1)
+      
+    }
+
+    setNumber('');
+
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.boldText}>{bodyText}</Text>
+      <TextInput style={styles.input} value={number} onChangeText={number =>setNumber(parseInt(number))} keyboardType='number-pad'></TextInput>
+      <Button title="make guess" onPress={buttonPressed}></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +57,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    
+    
   },
+  input: {
+    width: 200,
+    borderColor: 'black',
+    borderWidth: 1
+
+  },
+  boldText: {
+    fontWeight:"bold"
+  }
+
+
 });
